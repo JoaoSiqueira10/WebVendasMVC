23ca1c89a2137958fd0a201914994e8b949a3398
Joao Guilherme <joao.guilherme100@hotmail.com>
2022-07-13 13:05:25 -0300

    Primeiro model-controller-view - Departamento

--- WebVendasMvc/Controllers/HomeController.cs
+++ WebVendasMvc/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
-using WebVendasMvc.Models;
+using WebVendasMvc.Models.ViewModels;
 
 namespace WebVendasMvc.Controllers
 {
