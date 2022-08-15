55595ac04cffefffd7d41d5ac40da9a34bf4d1b1
Joao Guilherme <joao.guilherme100@hotmail.com>
2022-08-15 10:25:51 -0300

    VendedoresController

---
+++ WebVendasMvc/Controllers/VendedoresController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebVendasMvc.Controllers
+{
+    public class VendedoresController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
