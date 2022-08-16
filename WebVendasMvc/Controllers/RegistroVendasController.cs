66fdc1e26c630f65b6b46c7753b2bb0d1fc827b1
Joao Guilherme <joao.guilherme100@hotmail.com>
2022-08-16 16:19:02 -0300

    Preparando a navegacao das paginas

---
+++ WebVendasMvc/Controllers/RegistroVendasController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebVendasMvc.Controllers
+{
+    public class RegistroVendasController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult PesquisaSimples()
+        {
+            return View();
+        }
+
+        public IActionResult PesquisaGrupo()
+        {
+            return View();
+        }
+    }
+}
