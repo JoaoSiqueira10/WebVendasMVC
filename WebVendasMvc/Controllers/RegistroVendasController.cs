ce9ef533542167ed73327e5f69a14b21805b492b
Joao Guilherme <joao.guilherme100@hotmail.com>
2022-08-16 18:01:56 -0300

    Implementando pesquisa em agrupamento

--- WebVendasMvc/Controllers/RegistroVendasController.cs
+++ WebVendasMvc/Controllers/RegistroVendasController.cs
@@ -34,9 +34,20 @@ namespace WebVendasMvc.Controllers
             return View(resultado);
         }
 
-        public IActionResult PesquisaGrupo()
+        public async Task<IActionResult> PesquisaGrupo(DateTime? minDate, DateTime? maxDate)
         {
-            return View();
+            if (!minDate.HasValue)
+            {
+                minDate = new DateTime(DateTime.Now.Year, 1, 1);
+            }
+            if (!maxDate.HasValue)
+            {
+                maxDate = DateTime.Now;
+            }
+            ViewData["minDate"] = minDate.Value.ToString("yyyy-MM-dd");
+            ViewData["maxDate"] = maxDate.Value.ToString("yyyy-MM-dd");
+            var resultado = await _registroVendasService.FindByDateGrupoAsync(minDate, maxDate);
+            return View(resultado);
         }
     }
 }
--- WebVendasMvc/Models/RegistroVendas.cs
+++ WebVendasMvc/Models/RegistroVendas.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using WebVendasMvc.Models.Enums;
 
 namespace WebVendasMvc.Models
@@ -6,7 +7,11 @@ namespace WebVendasMvc.Models
     public class RegistroVendas
     {
         public int Id { get; set; }
+        
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime Data { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double Quantidade { get; set; }
         public EstatusVendas Status { get; set; }
         public Vendedor Vendedor { get; set; }
